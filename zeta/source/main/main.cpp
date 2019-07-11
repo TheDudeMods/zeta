e2472bdc67bc83b5674d1df237f9ecc76ed9422a
Camden Smallwood <camden.smallwood@gmail.com>
2019-07-11 15:55:10 -0400

    Update main.cpp

--- zeta/source/main/main.cpp
+++ zeta/source/main/main.cpp
@@ -4,10 +4,21 @@
 
 int main()
 {
+	//
+	// Allocate and initialize the reach cache file
+	//
+
 	auto file = new c_cache_file("C:\\Halo\\Reach\\maps\\m35.map");
 
+	//
+	// Get the object definition of 'objects\characters\spartans\spartans.biped'
+	//
+
 	auto object = file->get_tag_definition<s_object_definition>(0x36B1);
-	auto attachments = file->get_page_data<s_object_attachment>(object->attachments.address);
+	
+	//
+	// Dispose and deallocate the cache file
+	//
 
 	delete file;
 
