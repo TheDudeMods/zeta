7298e4f34d960cad689db597d0530edb732c60a6
Camden Smallwood <camden.smallwood@gmail.com>
2019-12-01 19:50:12 -0500

    Move 'cache_file_load' (test)

--- haloreach/source/cache/cache_files.cpp
+++ haloreach/source/cache/cache_files.cpp
@@ -8,84 +8,6 @@
 
 /* ---------- code */
 
-[[nodiscard]]
-c_cache_file_reach *cache_file_load()
-{
-	static char cache_file_path[1024];
-
-	//
-	// Look up the cache file to open
-	//
-
-	while (true)
-	{
-		puts("");
-		puts("Enter the path to a Halo cache file:");
-		printf("> ");
-
-		//
-		// Read the cache file path
-		//
-
-		csmemset(cache_file_path, 0, 1024);
-		fgets(cache_file_path, 1024, stdin);
-
-		auto newline = csstrchr(cache_file_path, '\n');
-		if (newline) *newline = '\0';
-
-		//
-		// If empty cache file path, start over
-		//
-
-		if (csstrlen(cache_file_path) == 0)
-			continue;
-
-		//
-		// Verify the cache file format
-		//
-
-		FILE *stream = nullptr;
-		if (fopen_s(&stream, cache_file_path, "rb+") == 0 && stream)
-		{
-			//
-			// Read limited cache file header info
-			//
-
-			struct {
-				tag header_signature;
-				long file_version;
-				long file_length;
-			} cache_file_info;
-
-			fseek(stream, 0, SEEK_SET);
-			fread(&cache_file_info, sizeof(cache_file_info), 1, stream);
-			fclose(stream);
-
-			//
-			// Check cache file header signature
-			//
-
-			if (cache_file_info.header_signature != k_cache_file_header_signature &&
-				_byteswap_ulong(cache_file_info.header_signature) != k_cache_file_header_signature)
-			{
-				puts("ERROR: Invalid cache file!");
-				continue;
-			}
-			else
-			{
-				puts("");
-				break;
-			}
-		}
-	}
-
-	//
-	// Allocate and load the cache file
-	//
-
-	return new c_cache_file_reach(cache_file_path);
-}
-
 bool s_cache_file_tag_group::is_in_group(tag group_tag) const
 {
 	if (group_tag == NONE)
@@ -292,4 +214,82 @@ ulonglong c_cache_file_reach::get_page_offset(ulong address)
 ulong c_cache_file_reach::make_page_offset(ulonglong address)
 {
 	return (ulong)((address + (m_header.virtual_base_address - 0x50000000)) / 4);
+}
+
+[[nodiscard]]
+c_cache_file_reach *cache_file_load()
+{
+	static char cache_file_path[1024];
+
+	//
+	// Look up the cache file to open
+	//
+
+	while (true)
+	{
+		puts("");
+		puts("Enter the path to a Halo cache file:");
+		printf("> ");
+
+		//
+		// Read the cache file path
+		//
+
+		csmemset(cache_file_path, 0, 1024);
+		fgets(cache_file_path, 1024, stdin);
+
+		auto newline = csstrchr(cache_file_path, '\n');
+		if (newline) *newline = '\0';
+
+		//
+		// If empty cache file path, start over
+		//
+
+		if (csstrlen(cache_file_path) == 0)
+			continue;
+
+		//
+		// Verify the cache file format
+		//
+
+		FILE *stream = nullptr;
+		if (fopen_s(&stream, cache_file_path, "rb+") == 0 && stream)
+		{
+			//
+			// Read limited cache file header info
+			//
+
+			struct {
+				tag header_signature;
+				long file_version;
+				long file_length;
+			} cache_file_info;
+
+			fseek(stream, 0, SEEK_SET);
+			fread(&cache_file_info, sizeof(cache_file_info), 1, stream);
+			fclose(stream);
+
+			//
+			// Check cache file header signature
+			//
+
+			if (cache_file_info.header_signature != k_cache_file_header_signature &&
+				_byteswap_ulong(cache_file_info.header_signature) != k_cache_file_header_signature)
+			{
+				puts("ERROR: Invalid cache file!");
+				continue;
+			}
+			else
+			{
+				puts("");
+				break;
+			}
+		}
+	}
+
+	//
+	// Allocate and load the cache file
+	//
+
+	return new c_cache_file_reach(cache_file_path);
 }
